9f0f353e05a985d3fb621a239e62692709a92770
Warwick <krugazul@gmail.com>
2023-08-14 20:04:29 +0400

    Merge changes

--- includes/patterns/testimonials/testimonial-1-column.php
+++ includes/patterns/testimonials/testimonial-1-column.php
@@ -20,7 +20,7 @@ return array(
 <!-- wp:quote {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"backgroundColor":"base","textColor":"contrast","className":"is-style-default"} -->
 <blockquote class="wp-block-quote is-style-default has-contrast-color has-base-background-color has-text-color has-background has-link-color" style="font-style:normal;font-weight:600"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"0","bottom":"0","left":"0"},"blockGap":"0"}},"layout":{"type":"default"}} -->
 <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"id":41956,"width":305,"height":88,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full is-resized"><img alt="" class="wp-image-41956" width="305" height="88"/></figure>
+<figure class="wp-block-image size-full is-resized"><img src="' . esc_url( get_theme_file_uri() ) . '/assets/images/lsxd-theme.png" alt="" class="wp-image-1414"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:separator {"style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|small"}}},"className":"is-style-default"} -->
