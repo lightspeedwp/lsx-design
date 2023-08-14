5e6b87dbfae2c9865b5ebe5dfcd4715c422ac8fa
Warwick <krugazul@gmail.com>
2023-08-14 15:34:11 +0400

    Changing quaternary colour to base #34

--- includes/patterns/pages/page-testimonials.php
+++ includes/patterns/pages/page-testimonials.php
@@ -41,8 +41,8 @@ return array(
     <hr class="wp-block-separator has-alpha-channel-opacity is-style-dots" style="margin-top:0;margin-bottom:var(--wp--preset--spacing--x-small)"/>
     <!-- /wp:separator -->
     
-    <!-- wp:quote {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"backgroundColor":"quaternary","textColor":"contrast","className":"is-style-default"} -->
-    <blockquote class="wp-block-quote is-style-default has-contrast-color has-quaternary-background-color has-text-color has-background has-link-color" style="font-style:normal;font-weight:600"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"0","bottom":"0","left":"0"},"blockGap":"0"}},"layout":{"type":"default"}} -->
+    <!-- wp:quote {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"backgroundColor":"base","textColor":"contrast","className":"is-style-default"} -->
+    <blockquote class="wp-block-quote is-style-default has-contrast-color has-base-background-color has-text-color has-background has-link-color" style="font-style:normal;font-weight:600"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"0","bottom":"0","left":"0"},"blockGap":"0"}},"layout":{"type":"default"}} -->
     <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"id":204,"sizeSlug":"large","linkDestination":"none"} -->
     <figure class="wp-block-image size-large"><img src="' . esc_url( get_theme_file_uri() ) . '/assets/images/LSX-logo-white-background.svg" alt="" class="wp-image-204"/></figure>
     <!-- /wp:image -->
@@ -65,8 +65,8 @@ return array(
     
     <!-- wp:columns {"verticalAlignment":null,"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"}}}} -->
     <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"bottom":"0"}}}} -->
-    <div class="wp-block-column is-vertically-aligned-top" style="padding-bottom:0"><!-- wp:quote {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"backgroundColor":"quaternary","textColor":"contrast","className":"is-style-default"} -->
-    <blockquote class="wp-block-quote is-style-default has-contrast-color has-quaternary-background-color has-text-color has-background has-link-color" style="font-style:normal;font-weight:600"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"0","bottom":"0","left":"0"},"blockGap":"0"}},"layout":{"type":"default"}} -->
+    <div class="wp-block-column is-vertically-aligned-top" style="padding-bottom:0"><!-- wp:quote {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"backgroundColor":"base","textColor":"contrast","className":"is-style-default"} -->
+    <blockquote class="wp-block-quote is-style-default has-contrast-color has-base-background-color has-text-color has-background has-link-color" style="font-style:normal;font-weight:600"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"0","bottom":"0","left":"0"},"blockGap":"0"}},"layout":{"type":"default"}} -->
     <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"id":204,"sizeSlug":"large","linkDestination":"none"} -->
     <figure class="wp-block-image size-large"><img src="' . esc_url( get_theme_file_uri() ) . '/assets/images/LSX-logo-white-background.svg" alt="" class="wp-image-204"/></figure>
     <!-- /wp:image -->
@@ -83,8 +83,8 @@ return array(
     <!-- /wp:column -->
     
     <!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"bottom":"0"}}}} -->
-    <div class="wp-block-column is-vertically-aligned-top" style="padding-bottom:0"><!-- wp:quote {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"backgroundColor":"quaternary","textColor":"contrast","className":"is-style-default"} -->
-    <blockquote class="wp-block-quote is-style-default has-contrast-color has-quaternary-background-color has-text-color has-background has-link-color" style="font-style:normal;font-weight:600"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"0","bottom":"0","left":"0"},"blockGap":"0"}},"layout":{"type":"default"}} -->
+    <div class="wp-block-column is-vertically-aligned-top" style="padding-bottom:0"><!-- wp:quote {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"backgroundColor":"base","textColor":"contrast","className":"is-style-default"} -->
+    <blockquote class="wp-block-quote is-style-default has-contrast-color has-base-background-color has-text-color has-background has-link-color" style="font-style:normal;font-weight:600"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"0","bottom":"0","left":"0"},"blockGap":"0"}},"layout":{"type":"default"}} -->
     <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"id":204,"sizeSlug":"large","linkDestination":"none"} -->
     <figure class="wp-block-image size-large"><img src="' . esc_url( get_theme_file_uri() ) . '/assets/images/LSX-logo-white-background.svg" alt="" class="wp-image-204"/></figure>
     <!-- /wp:image -->
@@ -105,8 +105,8 @@ return array(
     <!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small","bottom":"var:preset|spacing|large","left":"var:preset|spacing|x-small"}}},"layout":{"type":"constrained","contentSize":"1200px"},"extUtilities":[]} -->
     <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--large);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:columns {"verticalAlignment":null,"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"}}}} -->
     <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"bottom":"0"}}}} -->
-    <div class="wp-block-column is-vertically-aligned-top" style="padding-bottom:0"><!-- wp:quote {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"backgroundColor":"quaternary","textColor":"contrast","className":"is-style-default"} -->
-    <blockquote class="wp-block-quote is-style-default has-contrast-color has-quaternary-background-color has-text-color has-background has-link-color" style="font-style:normal;font-weight:600"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"0","bottom":"0","left":"0"},"blockGap":"0"}},"layout":{"type":"default"}} -->
+    <div class="wp-block-column is-vertically-aligned-top" style="padding-bottom:0"><!-- wp:quote {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"backgroundColor":"base","textColor":"contrast","className":"is-style-default"} -->
+    <blockquote class="wp-block-quote is-style-default has-contrast-color has-base-background-color has-text-color has-background has-link-color" style="font-style:normal;font-weight:600"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"0","bottom":"0","left":"0"},"blockGap":"0"}},"layout":{"type":"default"}} -->
     <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"id":204,"sizeSlug":"large","linkDestination":"none"} -->
     <figure class="wp-block-image size-large"><img src="' . esc_url( get_theme_file_uri() ) . '/assets/images/LSX-logo-white-background.svg" alt="" class="wp-image-204"/></figure>
     <!-- /wp:image -->
@@ -123,8 +123,8 @@ return array(
     <!-- /wp:column -->
     
     <!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"bottom":"0"}}}} -->
-    <div class="wp-block-column is-vertically-aligned-top" style="padding-bottom:0"><!-- wp:quote {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"backgroundColor":"quaternary","textColor":"contrast","className":"is-style-default"} -->
-    <blockquote class="wp-block-quote is-style-default has-contrast-color has-quaternary-background-color has-text-color has-background has-link-color" style="font-style:normal;font-weight:600"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"0","bottom":"0","left":"0"},"blockGap":"0"}},"layout":{"type":"default"}} -->
+    <div class="wp-block-column is-vertically-aligned-top" style="padding-bottom:0"><!-- wp:quote {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"backgroundColor":"base","textColor":"contrast","className":"is-style-default"} -->
+    <blockquote class="wp-block-quote is-style-default has-contrast-color has-base-background-color has-text-color has-background has-link-color" style="font-style:normal;font-weight:600"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"0","bottom":"0","left":"0"},"blockGap":"0"}},"layout":{"type":"default"}} -->
     <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"id":204,"sizeSlug":"large","linkDestination":"none"} -->
     <figure class="wp-block-image size-large"><img src="' . esc_url( get_theme_file_uri() ) . '/assets/images/LSX-logo-white-background.svg" alt="" class="wp-image-204"/></figure>
     <!-- /wp:image -->
