bcd5b942cab5daea035a29bb2d2dcaecf2d46eac
Warwick <krugazul@gmail.com>
2023-11-14 14:47:28 +0200

    Adding in a stylesheet for the WC blocks #61

---
+++ includes/classes/woocommerce/class-assets.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * The block styles file
+ *
+ * @package lsx-design
+ */
+
+namespace LSXD\Classes\WooCommerce;
+
+/**
+ * All the functions that run on the frontend and the rendering of the blocks.
+ *
+ * @package   LSX
+ * @author    LightSpeed
+ * @license   GPL3
+ * @link
+ * @copyright 2023 LightSpeed
+ */
+class Assets {
+
+	/**
+	 * Handles the conditional loading of the WooCommerce files.
+	 *
+	 * @var array
+	 */
+	private $assets = array();
+
+	/**
+	 * Contructor
+	 */
+	public function __construct() {
+		add_action( 'after_setup_theme', array( $this, 'enqueue_block_styles' ), 10 );
+	}
+
+	/**
+	 * Returns an array of the block assets.
+	 *
+	 * @return array
+	 */
+	private function get_assets() {
+		$this->assets = array(
+			'woocommerce/order-confirmation-totals' => array(
+				'handle' => 'lsxd-wc-order-confirmation-totals',
+				'src'    => get_template_directory_uri() . '/assets/css/woocommerce/order-confirmation-totals.css',
+				'path'   => get_template_directory() . '/assets/css/woocommerce/order-confirmation-totals.css',
+			),
+		);
+		return $this->assets;
+	}
+
+	/**
+	 * Registers our block specific styles.
+	 *
+	 * @return void
+	 */
+	public function enqueue_block_styles() {
+		foreach ( $this->get_assets() as $name => $asset ) {
+			wp_enqueue_block_style(
+				$name,
+				array(
+					'handle' => $asset['handle'],
+					'src'    => $asset['src'],
+					'path'   => $asset['path'],
+				),
+			);
+		}
+	}
+}
---
+++ includes/classes/woocommerce/class-woocommerce.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The core file
+ *
+ * @package lsx-design
+ */
+
+namespace LSXD\Classes\WooCommerce;
+
+/**
+ * The main file loading file for the WooCommerce related functions
+ *
+ * @package   LSX
+ * @author    LightSpeed
+ * @license   GPL3
+ * @link
+ * @copyright 2023 LightSpeed
+ */
+class WooCommerce {
+
+	/**
+	 * Undocumented variable
+	 *
+	 * @var Assets()
+	 */
+	public $assets;
+
+	/**
+	 * Contructor
+	 */
+	public function __construct() {
+		add_action( 'init', array( $this, 'init' ) );
+	}
+
+	/**
+	 * Loads the actions we need.
+	 *
+	 * @return void
+	 */
+	public function init() {
+		if ( function_exists( 'WC' ) ) {
+			$this->load_classes();
+		}
+	}
+
+	/**
+	 * Loads the classes
+	 */
+	public function load_classes() {
+		require get_template_directory() . '/includes/classes/woocommerce/class-assets.php';
+		$this->assets = new Assets();
+	}
+}
