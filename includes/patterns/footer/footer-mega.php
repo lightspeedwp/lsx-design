f96b76b3fe9c047f1170b86913ed33be361a30c0
Justinabes007 <103206213+Justinabes007@users.noreply.github.com>
2023-11-21 09:21:20 +0200

    Commit of Updated Footer files for Improved Mobile Design

--- includes/patterns/footer/footer-mega.php
+++ includes/patterns/footer/footer-mega.php
@@ -9,87 +9,137 @@ return array(
 	'title'      => __( 'Footer Mega ', 'lsx-design' ),
 	'categories' => array( 'footer' ),
 	'content'    => '
-<!-- wp:group {"align":"full","style":{"typography":{"fontSize":"18px"},"spacing":{"margin":{"top":"0px"},"padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="margin-top:0px;padding-top:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--x-large);font-size:18px"><!-- wp:columns {"align":"wide","style":{"elements":{"link":{"color":[]}}}} -->
-<div class="wp-block-columns alignwide has-link-color"><!-- wp:column {"width":"55%"} -->
-<div class="wp-block-column" style="flex-basis:55%"><!-- wp:heading {"level":4} -->
-<h4 id="our-company">' . esc_html__( 'Our Company', 'lsx-design' ) . '</h4>
-<!-- /wp:heading -->
-<!-- wp:paragraph -->
-<p>Quisque aliquam nisl quis metus taylor feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing vestibulum vitae gravida non diam.</p>
-<!-- /wp:paragraph -->
-<!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button {"style":{"border":{"radius":0}},"className":"is-style-fill"} -->
-<div class="wp-block-button is-style-fill"><a class="wp-block-button__link no-border-radius wp-element-button" href="#">' . esc_html__( 'Learn More', 'lsx-design' ) . '</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:column -->
-<!-- wp:column {"width":"15%"} -->
-<div class="wp-block-column" style="flex-basis:15%"><!-- wp:heading {"level":4} -->
-<h4 id="about-us">' . esc_html__( 'About Us', 'lsx-design' ) . '</h4>
-<!-- /wp:heading -->
-<!-- wp:list {"fontSize":"small"} -->
-<ul class="has-small-font-size"><!-- wp:list-item -->
-<li><a href="#">' . esc_html__( 'Start Here', 'lsx-design' ) . '</a></li>
-<!-- /wp:list-item -->
-<!-- wp:list-item -->
-<li><a href="#">' . esc_html__( 'Our Mission', 'lsx-design' ) . '</a></li>
-<!-- /wp:list-item -->
-<!-- wp:list-item -->
-<li><a href="#">' . esc_html__( 'Brand Guide', 'lsx-design' ) . '</a></li>
-<!-- /wp:list-item -->
-<!-- wp:list-item -->
-<li><a href="#">' . esc_html__( 'Newsletter', 'lsx-design' ) . '</a></li>
-<!-- /wp:list-item -->
-<!-- wp:list-item -->
-<li><a href="#">' . esc_html__( 'Accessibility', 'lsx-design' ) . '</a></li>
-<!-- /wp:list-item --></ul>
-<!-- /wp:list --></div>
-<!-- /wp:column -->
-<!-- wp:column {"width":"15%"} -->
-<div class="wp-block-column" style="flex-basis:15%"><!-- wp:heading {"level":4} -->
-<h4 id="services">' . esc_html__( 'Services', 'lsx-design' ) . '</h4>
-<!-- /wp:heading -->
-<!-- wp:list {"fontSize":"small"} -->
-<ul class="has-small-font-size"><!-- wp:list-item -->
-<li><a href="#">' . esc_html__( 'Web Design', 'lsx-design' ) . '</a></li>
-<!-- /wp:list-item -->
-<!-- wp:list-item -->
-<li><a href="#">' . esc_html__( 'Development', 'lsx-design' ) . '</a></li>
-<!-- /wp:list-item -->
-<!-- wp:list-item -->
-<li><a href="#">' . esc_html__( 'Copywriting', 'lsx-design' ) . '</a></li>
-<!-- /wp:list-item -->
-<!-- wp:list-item -->
-<li><a href="#">' . esc_html__( 'Marketing', 'lsx-design' ) . '</a></li>
-<!-- /wp:list-item -->
-<!-- wp:list-item -->
-<li><a href="#">' . esc_html__( 'Social Media', 'lsx-design' ) . '</a></li>
-<!-- /wp:list-item --></ul>
-<!-- /wp:list --></div>
-<!-- /wp:column -->
-<!-- wp:column {"width":"15%"} -->
-<div class="wp-block-column" style="flex-basis:15%"><!-- wp:heading {"level":4} -->
-<h4 id="connect">' . esc_html__( 'Connect', 'lsx-design' ) . '</h4>
-<!-- /wp:heading -->
-<!-- wp:list {"fontSize":"small"} -->
-<ul class="has-small-font-size"><!-- wp:list-item -->
-<li><a href="#">Facebook</a></li>
-<!-- /wp:list-item -->
-<!-- wp:list-item -->
-<li><a href="#">Instagram</a></li>
-<!-- /wp:list-item -->
-<!-- wp:list-item -->
-<li><a href="#">Twitter</a></li>
-<!-- /wp:list-item -->
-<!-- wp:list-item -->
-<li><a href="#">LinkedIn</a></li>
-<!-- /wp:list-item -->
-<!-- wp:list-item -->
-<li><a href="#">Dribbble</a></li>
-<!-- /wp:list-item --></ul>
-<!-- /wp:list --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
-<!-- /wp:group -->',
+	<!-- wp:group {"align":"full","style":{"typography":{"fontSize":"18px"},"spacing":{"margin":{"top":"0px","bottom":"0"},"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"0","right":"0"},"blockGap":"0"},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"backgroundColor":"base","textColor":"contrast","layout":{"type":"constrained"}} -->
+	<div class="wp-block-group alignfull has-contrast-color has-base-background-color has-text-color has-background has-link-color" style="margin-top:0px;margin-bottom:0;padding-top:var(--wp--preset--spacing--medium);padding-right:0;padding-bottom:var(--wp--preset--spacing--medium);padding-left:0;font-size:18px"><!-- wp:columns {"align":"wide","style":{"elements":{"link":{"color":[]}}}} -->
+	<div class="wp-block-columns alignwide has-link-color"><!-- wp:column {"width":"55%"} -->
+	<div class="wp-block-column" style="flex-basis:55%"><!-- wp:heading {"level":4,"fontSize":"large"} -->
+	<h4 class="wp-block-heading has-large-font-size" id="our-company">Company Heading</h4>
+	<!-- /wp:heading -->
+	
+	<!-- wp:paragraph -->
+	<p>Quisque aliquam nisl quis metus taylor feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing vestibulum vitae gravida non diam.</p>
+	<!-- /wp:paragraph -->
+	
+	<!-- wp:buttons -->
+	<div class="wp-block-buttons"><!-- wp:button {"style":{"border":{"radius":0}},"className":"is-style-cta"} -->
+	<div class="wp-block-button is-style-cta"><a class="wp-block-button__link no-border-radius wp-element-button" href="#">Learn More</a></div>
+	<!-- /wp:button --></div>
+	<!-- /wp:buttons --></div>
+	<!-- /wp:column -->
+	
+	<!-- wp:column {"width":"15%"} -->
+	<div class="wp-block-column" style="flex-basis:15%"><!-- wp:heading {"level":4} -->
+	<h4 class="wp-block-heading" id="about-us">About Us</h4>
+	<!-- /wp:heading -->
+	
+	<!-- wp:list {"fontSize":"small"} -->
+	<ul class="has-small-font-size"><!-- wp:list-item -->
+	<li><a href="#">Start Here</a></li>
+	<!-- /wp:list-item -->
+	
+	<!-- wp:list-item -->
+	<li><a href="#">Our Mission</a></li>
+	<!-- /wp:list-item -->
+	
+	<!-- wp:list-item -->
+	<li><a href="#">Brand Guide</a></li>
+	<!-- /wp:list-item -->
+	
+	<!-- wp:list-item -->
+	<li><a href="#">Newsletter</a></li>
+	<!-- /wp:list-item -->
+	
+	<!-- wp:list-item -->
+	<li><a href="#">Accessibility</a></li>
+	<!-- /wp:list-item --></ul>
+	<!-- /wp:list --></div>
+	<!-- /wp:column -->
+	
+	<!-- wp:column {"width":"15%"} -->
+	<div class="wp-block-column" style="flex-basis:15%"><!-- wp:heading {"level":4} -->
+	<h4 class="wp-block-heading" id="services">Services</h4>
+	<!-- /wp:heading -->
+	
+	<!-- wp:list {"fontSize":"small"} -->
+	<ul class="has-small-font-size"><!-- wp:list-item -->
+	<li><a href="#">Web Design</a></li>
+	<!-- /wp:list-item -->
+	
+	<!-- wp:list-item -->
+	<li><a href="#">Development</a></li>
+	<!-- /wp:list-item -->
+	
+	<!-- wp:list-item -->
+	<li><a href="#">Copywriting</a></li>
+	<!-- /wp:list-item -->
+	
+	<!-- wp:list-item -->
+	<li><a href="#">Marketing</a></li>
+	<!-- /wp:list-item -->
+	
+	<!-- wp:list-item -->
+	<li><a href="#">Social Media</a></li>
+	<!-- /wp:list-item --></ul>
+	<!-- /wp:list --></div>
+	<!-- /wp:column -->
+	
+	<!-- wp:column {"width":"15%"} -->
+	<div class="wp-block-column" style="flex-basis:15%"><!-- wp:heading {"level":4} -->
+	<h4 class="wp-block-heading" id="connect">Connect</h4>
+	<!-- /wp:heading -->
+	
+	<!-- wp:list {"fontSize":"small"} -->
+	<ul class="has-small-font-size"><!-- wp:list-item -->
+	<li><a href="#">Facebook</a></li>
+	<!-- /wp:list-item -->
+	
+	<!-- wp:list-item -->
+	<li><a href="#">Instagram</a></li>
+	<!-- /wp:list-item -->
+	
+	<!-- wp:list-item -->
+	<li><a href="#">Twitter</a></li>
+	<!-- /wp:list-item -->
+	
+	<!-- wp:list-item -->
+	<li><a href="#">LinkedIn</a></li>
+	<!-- /wp:list-item -->
+	
+	<!-- wp:list-item -->
+	<li><a href="#">Dribbble</a></li>
+	<!-- /wp:list-item --></ul>
+	<!-- /wp:list --></div>
+	<!-- /wp:column --></div>
+	<!-- /wp:columns --></div>
+	<!-- /wp:group -->
+	
+	<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"blockGap":"0"},"border":{"top":{"color":"var:preset|color|quinary","width":"1px"},"right":{"width":"0px","style":"none"},"bottom":{"width":"0px","style":"none"},"left":{"width":"0px","style":"none"}},"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"backgroundColor":"base","textColor":"contrast","className":"legal","layout":{"type":"constrained","contentSize":"1200px"}} -->
+	<div class="wp-block-group legal has-contrast-color has-base-background-color has-text-color has-background has-link-color" style="border-top-color:var(--wp--preset--color--quinary);border-top-width:1px;border-right-style:none;border-right-width:0px;border-bottom-style:none;border-bottom-width:0px;border-left-style:none;border-left-width:0px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small","padding":{"top":"0","bottom":"0","left":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small"},"margin":{"top":"0","bottom":"0"}},"border":{"width":"0px","style":"none"}},"className":"legal","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between","orientation":"horizontal"}} -->
+	<div class="wp-block-group legal" style="border-style:none;border-width:0px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--x-small);padding-bottom:0;padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"5px","padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small"}},"border":{"width":"0px","style":"none"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"left","verticalAlignment":"center"}} -->
+	<div class="wp-block-group" style="border-style:none;border-width:0px;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--x-small)"><!-- wp:site-title {"level":0,"style":{"typography":{"lineHeight":"0.6"},"spacing":{"padding":{"top":"0","bottom":"0"}}},"fontSize":"small"} /--></div>
+	<!-- /wp:group -->
+	
+	<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+	<div class="wp-block-group has-contrast-color has-text-color has-link-color"><!-- wp:paragraph {"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"var:preset|spacing|x-small"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"small"} -->
+	<p class="has-base-color has-text-color has-link-color has-small-font-size" style="padding-top:0;padding-right:var(--wp--preset--spacing--x-small);padding-bottom:0;padding-left:0"><a href="/terms-conditions/">Terms &amp; Conditions </a></p>
+	<!-- /wp:paragraph -->
+	
+	<!-- wp:paragraph {"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"small"} -->
+	<p class="has-base-color has-text-color has-link-color has-small-font-size" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><a href="/privacy-policy/">Privacy Policy</a></p>
+	<!-- /wp:paragraph --></div>
+	<!-- /wp:group -->
+	
+	<!-- wp:social-links {"iconColor":"base","iconColorValue":"#ffffff","iconBackgroundColor":"contrast","iconBackgroundColorValue":"#000000","openInNewTab":true,"size":"has-normal-icon-size","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"margin":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small"},"blockGap":{"top":"0","left":"5px"}}},"layout":{"type":"flex","flexWrap":"wrap","orientation":"horizontal","justifyContent":"space-between"}} -->
+	<ul class="wp-block-social-links has-normal-icon-size has-icon-color has-icon-background-color has-link-color" style="margin-top:var(--wp--preset--spacing--x-small);margin-bottom:var(--wp--preset--spacing--x-small)"><!-- wp:social-link {"url":"https://www.facebook.com/wanderlustjewelryZA","service":"facebook"} /-->
+	
+	<!-- wp:social-link {"url":"https://twitter.com/","service":"twitter"} /-->
+	
+	<!-- wp:social-link {"url":"https://www.youtube.com/","service":"youtube"} /-->
+	
+	<!-- wp:social-link {"url":"https://www.instagram.com/","service":"instagram"} /-->
+	
+	<!-- wp:social-link {"url":"https://za.pinterest.com/","service":"pinterest"} /--></ul>
+	<!-- /wp:social-links --></div>
+	<!-- /wp:group --></div>
+	<!-- /wp:group -->',
 );
